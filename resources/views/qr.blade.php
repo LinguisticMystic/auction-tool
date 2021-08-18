b3c7756a93b70a0b00bf1e3808772b0b1f4757cc
LinguisticMystic <aija.pastare@gmail.com>
2021-08-18 20:54:40 +0300

    QR

--- resources/views/qr.blade.php
+++ resources/views/qr.blade.php
@@ -1,6 +1,6 @@
 <title> {{ 'QR #'. $id }}</title>
 
-<h1>{{ 'QR #'. $id }}</h1>
+<h1>{{ '#'. $id }}</h1>
 
 <img class="auction-item" src="{{ asset('storage/' . $auctionItem->path_to_QR_image) }}">
 
