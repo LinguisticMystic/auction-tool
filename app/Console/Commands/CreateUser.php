b1556c85f4eca76d92329a1295cf20ce3fcc188f
LinguisticMystic <aija.pastare@gmail.com>
2021-08-20 19:37:40 +0300

    User creation Artisan command

---
+++ app/Console/Commands/CreateUser.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
+
+class CreateUser extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'create:user';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $name = $this->ask('Name: ');
+        $password = $this->secret('Password: ');
+
+        User::create([
+            'name' => $name,
+            'password' => Hash::make($password)
+        ]);
+
+        echo 'User '. $name . ' created.';
+
+        return 0;
+    }
+}
