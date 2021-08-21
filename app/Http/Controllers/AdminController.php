854ca8664b0e77d2d67cecbd73b96e4d7d547c10
LinguisticMystic <aija.pastare@gmail.com>
2021-08-21 08:18:17 +0300

    Auction items table in dashboard view fixed

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -26,23 +26,26 @@ class AdminController extends Controller
         $auctionItems = AuctionItem::all();
 
         foreach ($auctionItems as $auctionItem) {
-            //var_dump($auctionItem->id);
             $bids = Bid::where('auction_item_id', $auctionItem->id)->get();
 
-            $maxBidAmount = 0;
-            $maxBid = null;
+            if ($bids->count() > 0) {
+                $maxBidAmount = 0;
+                $maxBid = null;
 
-            foreach ($bids as $bid) {
-
-                if ($bid->bid_amount > $maxBidAmount) {
-                    $maxBid = $bid;
+                foreach ($bids as $bid) {
+                    if ($bid->bid_amount > $maxBidAmount) {
+                        $maxBid = $bid;
+                    }
+                    $auctionItemsWithHighestBidders[$auctionItem->id]['bid_amount'] = $maxBid->bid_amount;
+                    $auctionItemsWithHighestBidders[$auctionItem->id]['bidder_name'] = $maxBid->bidder_name;
+                    $auctionItemsWithHighestBidders[$auctionItem->id]['bidder_phone'] = $maxBid->bidder_phone;
                 }
-
+            } else {
+                $auctionItemsWithHighestBidders[$auctionItem->id]['bid_amount'] = 0;
+                $auctionItemsWithHighestBidders[$auctionItem->id]['bidder_name'] = '-';
+                $auctionItemsWithHighestBidders[$auctionItem->id]['bidder_phone'] = '-';
             }
 
-            $auctionItemsWithHighestBidders[$auctionItem->id]['bid_amount'] = $maxBid->bid_amount;
-            $auctionItemsWithHighestBidders[$auctionItem->id]['bidder_name'] = $maxBid->bidder_name;
-            $auctionItemsWithHighestBidders[$auctionItem->id]['bidder_phone'] = $maxBid->bidder_phone;
         }
 
         return view('admin.dashboard',
