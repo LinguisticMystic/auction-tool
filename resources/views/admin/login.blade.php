57a931cb8f574ea1fe8a5556004a5e1440a6670d
LinguisticMystic <aija.pastare@gmail.com>
2021-08-22 09:03:24 +0300

    Class added to admin login fields

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -8,28 +8,29 @@
     <form action="/login" method="post">
         @csrf
 
-        <br>
-        <label>{{__('forms.name')}}: </label>
-        <input type="text" name="name">
-        <div class="form-error">
-            <strong>{{ $errors->first('name') }}</strong>
+        <div class="form">
+            <br>
+            <label>{{__('forms.name')}}: </label>
+            <input type="text" name="name">
+            <div class="form-error">
+                <strong>{{ $errors->first('name') }}</strong>
+            </div>
+
+            <br>
+
+            <label>{{__('forms.password')}}: </label>
+            <input type="password" name="password">
+            <div class="form-error">
+                <strong>{{ $errors->first('password') }}</strong>
+            </div>
+
+            <div class="form-error">
+                <strong>{{ $errors->first('failed_login') }}</strong>
+            </div>
+
+            <br>
+
+            <input type="submit" value="{{__('controls.log_in')}}">
         </div>
-
-        <br>
-
-        <label>{{__('forms.password')}}: </label>
-        <input type="password" name="password">
-        <div class="form-error">
-            <strong>{{ $errors->first('password') }}</strong>
-        </div>
-
-        <div class="form-error">
-            <strong>{{ $errors->first('failed_login') }}</strong>
-        </div>
-
-        <br>
-
-        <input type="submit" value="{{__('controls.log_in')}}">
-
     </form>
 @endsection
